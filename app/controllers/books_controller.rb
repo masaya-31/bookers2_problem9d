4b4b10ab20bd46434a89c29e93377910ee0bc5a2
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-10 04:11:00 +0000

    [Fix]booksコントローラのアクセス制限修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
@@ -22,15 +23,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    book = Book.find(params[:id])
-    unless book.user_id == current_user.id
-      redirect_to books_path
-    end
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -39,7 +34,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -48,6 +42,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
