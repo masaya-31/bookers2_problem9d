c50e76c1fe2ee5c52cd7f6032493d83a484efd50
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-15 09:19:48 +0000

    [Finish]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,28 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings_users, through: :followers, source: :followed
+  has_many :followers_users, through: :followeds, source: :follower
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings_users.include?(user)
   end
 end
