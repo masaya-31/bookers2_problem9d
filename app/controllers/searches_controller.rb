3b6f41cbd2bcdd7ce773feed245280debdab67b2
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-16 06:30:46 +0000

    [Finish]応用課題5

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,14 @@
+class SearchesController < ApplicationController
+
+  def search
+    @range = params[:range]
+    @search = params[:search]
+    @word = params[:word]
+
+    if @range == "User"
+      @users = User.looks(@search, @word)
+    else
+      @books = Book.looks(@search, @word)
+    end
+  end
+end
