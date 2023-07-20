480b4c7d2d42c442e24022c55370c7b1721fc52f
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-20 05:04:37 +0000

    [Finish]応用課題7d

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,17 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 
-import "../stylesheets/application" 
+import "../stylesheets/application"
+
+window.$ = window.jQuery = require('jquery');
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
